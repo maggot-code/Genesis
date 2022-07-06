94cd910fffbfc76241f0586294444a38a00113a8
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-07-06 23:41:51 +0800

    fix :-1:  npm package config

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @FilePath: /Genesis/index.js
+ * @Author: maggot-code
+ * @Date: 2022-07-06 23:32:29
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-07-06 23:32:45
+ * @Description:
+ */
+console.log('genesis');
